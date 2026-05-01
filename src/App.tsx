4674e37bc1ebdb94431b79640bdd9c36edb0af4b
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-01 23:26:32 +0300

    chore: install prettier & format by prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-	return <h1 className="center">Hello world from Vite + React</h1>;
+  return <h1 className="center">Hello world from Vite + React </h1>;
 }
 
 export default App;
