55dcc717b18448a8fbc3b79dc199554eeb7f4eb0
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-02 14:52:58 +0300

    chore: install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <h1 className="center">Hello world from Vite + React </h1>;
+  return (
+    <h1 className="text-sky-800 text-4xl center">
+      Hello world from Vite + React{' '}
+    </h1>
+  );
 }
 
 export default App;
