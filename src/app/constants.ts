278dccc3cebf45953c12d72b85b5d27bf8aaacdd
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-04 08:42:44 +0300

    feat: add fetch addData

---
+++ src/app/constants.ts
@@ -0,0 +1,7 @@
+import type { Url } from "./index";
+
+export const LINKS: Url = {
+  characters: 'https://swapi.py4e.com/api/people/?',
+};
+
+export const CARDS_PER_PAGE: number = 10;
---
+++ src/app/index.ts
@@ -0,0 +1,2 @@
+export {LINKS, CARDS_PER_PAGE} from './constants';
+export type {Url, FetchError, Card, DataType} from './types';
