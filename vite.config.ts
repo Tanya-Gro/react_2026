84073b9f01c9f6c0ce00d1bc80ab976aa5b39b0d
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-18 22:12:14 +0300

    chore: add tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }), 
+    react(), 
+    tailwindcss()],
 });
