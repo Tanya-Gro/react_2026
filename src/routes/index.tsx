5d06f7100466609dd3dd2f327a257cc349cac17e
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-19 18:26:53 +0300

    feat: add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { Home } from '../pages';
+import { Home } from 'pages';
 
 type SearchParams = {
   search?: string;
   page?: number;
+  details?: string;
 };
 
 export const Route = createFileRoute('/')({
   validateSearch: (search: Record<string, unknown>): SearchParams => ({
     search: typeof search.search === 'string' ? search.search : '',
     page: Number(search.page) > 0 ? Number(search.page) : 1,
+    details: typeof search.details === 'string' ? search.details : undefined,
   }),
   component: Home,
 });
