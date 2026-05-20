5b41a84366643535375b75b1610cdea93f466a1e
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-20 11:37:22 +0300

    refactor: add errorComponent into routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,16 @@
 import { createRootRoute } from '@tanstack/react-router';
+import { ErrorBoundary } from 'components';
 import { RootLayout } from 'components/RootLayout';
 import { NotFound } from 'pages';
 
 export const Route = createRootRoute({
   component: RootLayout,
   notFoundComponent: NotFound,
+  errorComponent: ({ error }) => (
+    <ErrorBoundary>
+      {((): React.JSX.Element => {
+        throw error instanceof Error ? error : new Error(String(error));
+      })()}
+    </ErrorBoundary>
+  ),
 });
