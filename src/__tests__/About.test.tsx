e2c77c052e61cfd42c307fc139312e17628675ca
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-23 10:07:28 +0300

    test: remove unused router spy

--- src/__tests__/About.test.tsx
+++ src/__tests__/About.test.tsx
@@ -1,17 +1,12 @@
 import { screen } from '@testing-library/react';
-import { router } from 'app';
 import { renderWithRouter } from './test-utils/renderWithRouter';
 
 describe('About Component', () => {
   it('navigates to the About page', async () => {
-    const navigateSpy = vi.spyOn(router, 'navigate');
-
     await renderWithRouter({ route: '/about' });
 
     expect(
       await screen.findByRole('link', { name: /Rolling Scopes School/i })
     ).toBeInTheDocument();
-
-    navigateSpy.mockRestore();
   });
 });
