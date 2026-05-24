6e9d644619a7b1d52392a23d14490559003cc080
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-24 21:24:14 +0300

    chore: add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
+    tsconfigPaths(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
