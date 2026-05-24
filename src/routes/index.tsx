88edcd9d210469a2f7e6bbd1b3b50b6eff9f15af
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-24 21:42:24 +0300

    refactor: route add Math.trunk for page params

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ type SearchParams = {
 export const Route = createFileRoute('/')({
   validateSearch: (search: Record<string, unknown>): SearchParams => ({
     search: typeof search.search === 'string' ? search.search : '',
-    page: Number(search.page) > 0 ? Number(search.page) : 1,
+    page: Number(search.page) > 0 ? Math.trunc(Number(search.page)) : 1,
     details: typeof search.details === 'string' ? search.details : undefined,
   }),
   component: Home,
