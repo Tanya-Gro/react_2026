bee00acb04ee1c3c7c5a08554290bb4b99030294
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-24 09:28:23 +0300

    refactor: RootLayout, add semantic tag nav & TSR render in dev mode

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -5,17 +5,22 @@ import { Footer } from 'components';
 export const RootLayout = (): React.JSX.Element => {
   return (
     <>
-      <header className="p-4 flex gap-x-8 text-xl border-b-2 border-b-mist-300">
-        <Link to="/" className="[&.active]:font-bold [&.active]:underline">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold [&.active]:underline">
-          About
-        </Link>
+      <header className="p-4 border-b-2 border-b-mist-300">
+        <nav className="flex gap-x-8 text-xl">
+          <Link to="/" className="[&.active]:font-bold [&.active]:underline">
+            Home
+          </Link>{' '}
+          <Link
+            to="/about"
+            className="[&.active]:font-bold [&.active]:underline"
+          >
+            About
+          </Link>
+        </nav>
       </header>
       <Outlet />
       <Footer />
-      <TanStackRouterDevtools />
+      {import.meta.env.DEV && <TanStackRouterDevtools />}
     </>
   );
 };
