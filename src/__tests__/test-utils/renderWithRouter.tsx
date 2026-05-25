cf4a3c7c98eea4b586b81a924792862d42c73acf
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-25 11:20:27 +0300

    test: add store in test utils

--- src/__tests__/test-utils/renderWithRouter.tsx
+++ src/__tests__/test-utils/renderWithRouter.tsx
@@ -5,14 +5,26 @@ import {
 } from '@tanstack/react-router';
 import { render, type RenderResult } from '@testing-library/react';
 import { routeTree } from 'src/routeTree.gen';
+import { configureStore, type EnhancedStore } from '@reduxjs/toolkit';
+import { selectedCardsReducer } from 'features';
+import { Provider } from 'react-redux';
 
 type Options = {
   route?: string;
 };
 
+const createTestStore = (): EnhancedStore => {
+  return configureStore({
+    reducer: {
+      selectedCards: selectedCardsReducer,
+    },
+  });
+};
+
 export async function renderWithRouter(
   options: Options = {}
 ): Promise<RenderResult> {
+  const store = createTestStore();
   const { route = '/' } = options;
 
   const testRouter = createRouter({
@@ -24,5 +36,9 @@ export async function renderWithRouter(
 
   await testRouter.load();
 
-  return render(<RouterProvider router={testRouter} />);
+  return render(
+    <Provider store={store}>
+      <RouterProvider router={testRouter} />
+    </Provider>
+  );
 }
