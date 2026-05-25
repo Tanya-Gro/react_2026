d719fff33c3329a12ac8d530bafe746b8b6059ca
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-26 02:52:57 +0300

    refactor: downloadCards add cards information

--- src/helpers/downloadCards.ts
+++ src/helpers/downloadCards.ts
@@ -1,11 +1,17 @@
 import type { Card } from 'app';
 
 export const downloadCards = (selectedCards: [string, Card][]): string => {
-  const csvHeader = 'id,name,imageURL\n';
+  const SEPARATOR = '\n-----------------\n';
+  const csvHeader =
+    'ID, Name, Image URL, Height, Mass, Hair color, Skin, Eye color, Birth year, Gender:' +
+    SEPARATOR;
 
   const csvRows = selectedCards
-    .map(([id, card]) => `${id},"${card.name}","${card.url}"`)
-    .join('\n');
+    .map(
+      ([id, card]) =>
+        `ID: ${id}, Name: ${card.name}, Image URL: ${card.url}, Height: ${card.height}, Mass: ${card.mass}, Hair color: ${card.hair_color}, Skin: ${card.skin_color}, Eye color: ${card.eye_color}, Birth year: ${card.birth_year}, Gender: ${card.gender}`
+    )
+    .join(SEPARATOR);
 
   const csvContent = csvHeader + csvRows;
 
--- src/mocks/data/blob.ts
+++ src/mocks/data/blob.ts
@@ -1,3 +1,7 @@
+const SEPARATOR = '\n-----------------\n';
+
 export const BLOB =
-  'id,name,imageURL\n' +
-  '1,"Luke Skywalker","https://swapi.py4e.com/api/people/1/"';
+  'ID, Name, Image URL, Height, Mass, Hair color, Skin, Eye color, Birth year, Gender:' +
+  SEPARATOR +
+  'ID: 1, Name: Luke Skywalker, Image URL: https://swapi.py4e.com/api/people/1/, ' +
+  'Height: 172, Mass: 77, Hair color: blond, Skin: fair, Eye color: blue, Birth year: 19BBY, Gender: male';
