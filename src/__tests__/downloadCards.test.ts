48441c1c29f22d18682f305d02251c281d3eee16
Tanya-Gro <tarpel.gta@gmail.com>
2026-05-25 14:27:19 +0300

    test: add downloadCards

---
+++ src/__tests__/downloadCards.test.ts
@@ -0,0 +1,52 @@
+import { BLOB } from 'mocks';
+import { people } from 'mocks';
+import { downloadCards } from 'helpers';
+
+vi.stubGlobal('URL', {
+  createObjectURL: vi.fn(() => 'blob:mock-url'),
+  revokeObjectURL: vi.fn(),
+});
+
+describe('handleDownload', () => {
+  const createObjectURLSpy = vi.spyOn(URL, 'createObjectURL');
+
+  async function readBlobAsText(blob: Blob): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (e: ProgressEvent<FileReader>): void => {
+        const result = e.target?.result;
+
+        if (typeof result === 'string') {
+          resolve(result);
+        } else {
+          reject(new Error('FileReader result is not of type string'));
+        }
+      };
+
+      reader.onerror = (): void => {
+        reject(new Error('FileReader failed to read blob'));
+      };
+
+      reader.readAsText(blob);
+    });
+  }
+
+  it('should create a Blob with the actual CSV content', async () => {
+    downloadCards([['1', people.results[0]]]);
+
+    expect(createObjectURLSpy).toHaveBeenCalledTimes(1);
+
+    const blobArgument = createObjectURLSpy.mock.calls[0][0];
+
+    expect(blobArgument).toBeInstanceOf(Blob);
+
+    if (blobArgument instanceof Blob) {
+      expect(blobArgument.type).toBe('text/csv');
+      const csvContent = await readBlobAsText(blobArgument);
+      expect(csvContent).toEqual(BLOB);
+    } else {
+      throw new Error('blobArgument is not of type Blob');
+    }
+  });
+});
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -2,3 +2,4 @@ export { server } from './server';
 export { handlers } from './handlers';
 export { default as people } from './data/people.json';
 export { default as detail } from './data/detail.json';
+export { BLOB } from './data/blob';
