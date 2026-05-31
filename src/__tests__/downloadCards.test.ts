0cfa2c5f6de35936d23bf7b1d9862fbd22d9ad10
Tanya-Gro <tarpel.gta@gmail.com>
2026-06-01 01:37:30 +0300

    refactor: test handleDownload

--- src/__tests__/downloadCards.test.ts
+++ src/__tests__/downloadCards.test.ts
@@ -30,36 +30,14 @@ async function readBlobAsText(blob: Blob): Promise<string> {
 }
 
 describe('handleDownload', () => {
-  let createObjectURLSpy = vi.spyOn(URL, 'createObjectURL');
-
-  beforeEach(() => {
-    vi.stubGlobal('URL', {
-      createObjectURL: vi.fn(() => 'blob:mock-url'),
-      revokeObjectURL: vi.fn(),
-    });
-
-    createObjectURLSpy = vi.spyOn(URL, 'createObjectURL');
-  });
-
-  afterEach(() => {
-    vi.unstubAllGlobals();
-  });
-
   it('should create a Blob with the actual CSV content', async () => {
-    downloadCards([['1', people.results[0]]]);
+    const blob = downloadCards([['1', people.results[0]]]);
 
-    expect(createObjectURLSpy).toHaveBeenCalledTimes(1);
+    expect(blob).toBeInstanceOf(Blob);
+    expect(blob.type).toBe('text/csv;charset=utf-8');
 
-    const blobArgument = createObjectURLSpy.mock.calls[0][0];
+    const csvContent = await readBlobAsText(blob);
 
-    expect(blobArgument).toBeInstanceOf(Blob);
-
-    if (blobArgument instanceof Blob) {
-      expect(blobArgument.type).toBe('text/csv');
-      const csvContent = await readBlobAsText(blobArgument);
-      expect(csvContent).toEqual(BLOB);
-    } else {
-      throw new TypeError('blobArgument is not of type Blob');
-    }
+    expect(csvContent).toEqual(BLOB);
   });
 });
--- src/mocks/data/blob.ts
+++ src/mocks/data/blob.ts
@@ -1,7 +1,3 @@
-const SEPARATOR = '\n-----------------\n';
-
 export const BLOB =
-  'ID, Name, Image URL, Height, Mass, Hair color, Skin, Eye color, Birth year, Gender:' +
-  SEPARATOR +
-  'ID: 1, Name: Luke Skywalker, Image URL: https://swapi.py4e.com/api/people/1/, ' +
-  'Height: 172, Mass: 77, Hair color: blond, Skin: fair, Eye color: blue, Birth year: 19BBY, Gender: male';
+  'ID,Name,Image URL,Height,Mass,Hair Color,Skin Color,Eye Color,Birth Year,Gender\n' +
+  '1,"Luke Skywalker","https://swapi.py4e.com/api/people/1/","172","77","blond","fair","blue","19BBY","male"';
