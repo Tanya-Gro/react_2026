05f04fc72c8ea1bc9e67d5c2247f5f2c2c5af181
Tanya-Gro <tarpel.gta@gmail.com>
2026-06-19 23:12:04 +0300

    refactor: add root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,22 @@
+import { Metadata } from 'next';
 import './index.css';
+
+export const metadata: Metadata = {
+  title: 'React App 2026',
+  description:
+    'My App is is an educational project for the React 2026 course at RS School.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-white text-mist-700">
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
