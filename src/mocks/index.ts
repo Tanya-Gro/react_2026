ccd189ce2286686d633de96ecaf51b1bd8fa2e5c
Tanya-Gro <tarpel.gta@gmail.com>
2026-06-21 21:41:05 +0300

    test: remove

--- src/mocks/index.ts
+++
@@ -1,5 +0,0 @@
-export { server } from './server';
-export { handlers } from './handlers';
-export { default as people } from './data/people.json';
-export { default as detail } from './data/detail.json';
-export { BLOB } from './data/blob';
--- src/mocks/server.ts
+++
@@ -1,4 +0,0 @@
-import { setupServer, type SetupServer } from 'msw/node';
-import { handlers } from './handlers';
-
-export const server: SetupServer = setupServer(...handlers);
